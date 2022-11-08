bbe0d16c4c37b75d7dd1af95db6f6d033c32ce5e
juancrls <juan1112@outlook.com.br>
2022-11-07 23:40:51 -0300

    fix: adjust on text-area component inheritance

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -8,43 +8,4 @@ export default class HeaderHeaderComponent extends Component {
   constructor(...args) {
     super(...args);
   }
-  
-  @tracked queryResult;
-  @tracked cnpjInput;
-  @service cnpjQuery;
-  @service store;
-
-  @action
-  addCnpjInput(e) {
-    this.cnpjInput = e.target.value;
-    console.log("on input trigger", this.cnpjInput)
-  }
-
-  @task
-  *getData() { // fake API
-    this.queryResult = "";
-
-    let response = yield fetch('/api/data.json');
-    let { data } = yield response.json();
-
-    data.map(obj => {
-        if(obj.legalEntity.federalTaxNumber.match(/\d/g).join("") == this.cnpjInput.match(/\d/g).join("")) { // remover tratamento com match (usar mask)
-            this.queryResult = obj.legalEntity;
-        }
-        // console.log(Object.keys(obj.legalEntity))
-    })
-    
-    console.log('data received: ', data);
-    console.log("actual input: ", this.cnpjInput);
-    console.log("data: ", this.queryResult);
-
-    // return parsed;
-  }
-
-  // @task
-  // *getData() { // for API requests
-  //     this.data = yield this.store.findRecord('cnpjQuery', "60.746.948/0001-12").then(res => {
-  //         console.log("data", res)
-  //     })
-  // }
 }
--- app/controllers/cnpj-query.js
+++ app/controllers/cnpj-query.js
@@ -1,12 +1,46 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
 
 export default class CnpjQueryController extends Controller {
-    // @tracked cnpjInput = "";
+    @tracked cnpjInput = "";
+    @tracked queryResult;
+    @service store;
     
-    // @action 
-    // addCnpjInput(input) {
-    //     this.cnpjInput = input;
+    // User input
+    @action 
+    addCnpjInput(e) {
+        this.cnpjInput = e.target.value;
+        console.log("on input trigger", this.cnpjInput)
+    }
+    // Data 
+    @task
+    *getData() { // fake API
+      this.queryResult = "";
+  
+      let response = yield fetch('/api/data.json');
+      let { data } = yield response.json();
+  
+      data.map(obj => {
+          if(obj.legalEntity.federalTaxNumber.match(/\d/g).join("") == this.cnpjInput.match(/\d/g).join("")) { // remover tratamento com match (usar mask)
+              this.queryResult = obj.legalEntity;
+          }
+          // console.log(Object.keys(obj.legalEntity))
+      })
+      
+      console.log('data received: ', data);
+      console.log("actual input: ", this.cnpjInput);
+      console.log("data: ", this.queryResult);
+  
+      // return parsed;
+    }
+  
+    // @task
+    // *getData() { // for API requests
+    //     this.data = yield this.store.findRecord('cnpjQuery', "60.746.948/0001-12").then(res => {
+    //         console.log("data", res)
+    //     })
     // }
 }
