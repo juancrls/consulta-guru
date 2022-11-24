0341b84d7aef8706cdbc6aac06dfd641e22e11b5
juancrls <juan1112@outlook.com.br>
2022-11-24 19:48:46 -0300

    style: added spinner and adjust on footer styles

--- app/adapters/cnpj-query.js
+++ app/adapters/cnpj-query.js
@@ -2,7 +2,6 @@ import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class CnpjQueryAdapter extends RESTAdapter {
   urlForFindRecord(query) {
-    console.log('adapter - query', query);
     query = query.match(/\d/g).join('');
     let url = `https://api.nfse.io/LegalEntities/Basicinfo/taxNumber/${query}`;
     return url;
